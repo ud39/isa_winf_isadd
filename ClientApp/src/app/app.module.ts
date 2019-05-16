7cb37ead4053999f6dfbbf64003c7d4aed69292e
JUTI <juti@boottawong.de>
2019-05-16 12:22:05 +0200

    # Documentation
    - add directories {BPMN HERM SiteLang Templates}
    - update Tex
    - add .jpg .png of HERM SiteLang Web Templates

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
@@ -23,15 +23,16 @@ import { RegistrationComponent } from './registration/registration.component';
     RegistrationComponent
   ],
   imports: [
-    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
+    BrowserModule.withServerTransition({appId: 'ng-cli-universal'}),
     HttpClientModule,
     FormsModule,
     RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
-      { path: 'registration', component: RegistrationComponent}
-    ])
+      {path: '', component: HomeComponent, pathMatch: 'full'},
+      {path: 'counter', component: CounterComponent},
+      {path: 'fetch-data', component: FetchDataComponent},
+      {path: 'registration', component: RegistrationComponent}
+    ]),
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- ClientApp/src/app/registration/registration.component.ts
+++ ClientApp/src/app/registration/registration.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
   styleUrls: ['./registration.component.css']
 })
-export class RegistrationComponent implements OnInit {
 
-  constructor() { }
+export class RegistrationComponent implements OnInit {
+  registerForm: FormGroup;
+  loading = false;
+  submitted = false;
+
+  constructor(
+    private formBuilder: FormBuilder,
+  )
+  {
+
+  }
 
   ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      Email: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      passwordTypeAgain: ['', [Validators.required, Validators.minLength(6)]]
+    });
   }
+
+  get formForRegister() { return this.registerForm.controls; }
 
 }
