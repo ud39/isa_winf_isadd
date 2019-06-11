71a80f26ecfa9d94b614f64ca81295a823de044b
JUTI <juti@boottawong.de>
2019-06-11 14:36:09 +0200

    #Front-End
    CheckBox-Component-Equipment
    Add FormControl

--- ClientApp/src/app/search/checkbox-equipment/checkbox-equipment.component.ts
+++ ClientApp/src/app/search/checkbox-equipment/checkbox-equipment.component.ts
@@ -3,6 +3,7 @@ import {CheckboxComponent} from "../checkbox/checkbox.component";
 import {Router} from "@angular/router";
 import {MatCheckbox} from "@angular/material";
 import {CheckBoxesService} from "../../services/checkboxes.service";
+import {FormControl} from "@angular/forms";
 
 @Component({
   selector: 'app-checkbox-equipment',
@@ -15,13 +16,13 @@ export class CheckboxEquipmentComponent implements OnInit {
   @ViewChildren('cb') cbs : QueryList<MatCheckbox>;
 
   constructor(private router:Router, private checkBoxService: CheckBoxesService) { }
-
+  myControl = new FormControl();
   getCheckBoxes() : JSON{
     return this.checkBoxService.getCheckBoxesValues(this.cbs.toArray());
   }
 
   clear() {
-    this.checkBoxService.unselectCheckBoxes(this.cbs);
+    this.checkBoxService.unselectCheckBoxes(this.cbs,this.myControl);
   }
 
   ngOnInit() {
