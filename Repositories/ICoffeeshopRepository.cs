aee36b3a5adf75a944c30ce4d032a3776dc49f6e
bas <stu114069@mail.uni-kiel.de>
2019-06-17 21:39:38 +0200

    #Backend ID for Coffee_shop is now aritificial: "coffee_shop_id" init.sql script fixed

---
+++ Repositories/ICoffeeshopRepository.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using WinfADD.Models;
+
+namespace WinfADD.Repositories
+{
+    public interface ICoffeeShopRepository : ITableRepository<CoffeeShop>
+    {
+        
+        Task<CoffeeShop> GetById(int id);
+    }
+}
