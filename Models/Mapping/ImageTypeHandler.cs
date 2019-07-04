252b1fd9fedeb21484d3a0dda8928a6e1e2f2a1c
jwi <jwi@informatik.uni-kiel.de>
2019-07-04 16:16:43 +0200

    #Backend stuff

---
+++ Models/Mapping/ImageTypeHandler.cs
@@ -0,0 +1,21 @@
+using System.Data;
+using Dapper;
+
+namespace WinfADD.Models.Mapping
+{
+    public class ImageTypeHandler : SqlMapper.TypeHandler<Image>
+    {
+
+        //handles data to save into DB
+        public override void SetValue(IDbDataParameter parameter, Image value)
+        {
+            parameter.Value = new Image{FileName = value.FileName.ToString(), ContentType = value.ContentType.ToString()};
+        }
+
+        //deserialized data from DB into object
+        public override Image Parse(object value)
+        {
+            return (Image) value;
+        }
+    }
+}
